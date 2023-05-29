8e74042d426e266322783c7ffff6733aa269a6e9
Kwabena Boadi Sapong <kwabenasapong@users.noreply.github.com>
2023-05-29 14:33:59 +0000

    Add project files.

---
+++ HelloASPNet/Controllers/ProductsController.cs
@@ -0,0 +1,23 @@
+﻿using MakerCrafts.Website.Models;
+using MakerCrafts.WebSite.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MakerCrafts.Website.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        public ProductsController(JsonFileProductService productService) 
+        {
+            this.ProductService = productService;
+        }
+        public JsonFileProductService ProductService { get; }
+        [HttpGet]
+        public IEnumerable<Product> Get()
+        {
+            return ProductService.GetProducts();
+        }
+    }
+}
---
+++ HelloASPNet/Pages/Index.cshtml.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using MakerCrafts.WebSite.Services;
+using MakerCrafts.Website.Models;
+
+namespace MakerCrafts.WebSite.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        public JsonFileProductService ProductService;
+        public IEnumerable<Product> Products { get; private set; }
+
+        public IndexModel(ILogger<IndexModel> logger,
+            JsonFileProductService productService)
+        {
+            _logger = logger;
+            ProductService = productService;
+        }
+
+        public void OnGet()
+        {
+            Products = ProductService.GetProducts();
+        }
+    }
+}
