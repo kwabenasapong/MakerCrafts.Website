4dc22d74fa15d6524b4e945312d1e772f57ede36
Kwabena Boadi Sapong <kwabenasapong@users.noreply.github.com>
2023-05-29 14:33:59 +0000

    Add project files.

---
+++ HelloASPNet/Controllers/ProductsController.cs
@@ -0,0 +1,34 @@
+﻿using MakerCrafts.Website.Models;
+using MakerCrafts.WebSite.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MakerCrafts.Website.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        public ProductsController(JsonFileProductService productService) 
+        {
+            this.ProductService = productService;
+        }
+        public JsonFileProductService ProductService { get; }
+        [HttpGet]
+        public IEnumerable<Product> Get()
+        {
+            return ProductService.GetProducts();
+        }
+
+
+        [Route("Rating")]
+        [HttpGet]
+        public ActionResult Get(
+            [FromQuery] string ProductId,
+            [FromQuery] int Rating) 
+        {
+            ProductService.AddRatings(ProductId, Rating);
+            return Ok();
+        }
+    }
+}
